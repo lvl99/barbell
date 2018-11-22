9891fdcb9d65f11202e94e35a53e7d5a7a94fcb8
Matt Scheurich <matt@lvl99.com>
2018-11-22 19:49:53 +0100

    split up cmd, runner and reporter logic and made it changeable if people want to make their own custom runners/reporters

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const pkg = require("./package.json");
 const program = require("commander");
-const barbell = require("./lib/barbell");
+const barbell = require("./lib/cmd");
 
 function collect(val, memo = []) {
   if (!(memo instanceof Array)) {
@@ -19,17 +19,26 @@ program
   .option(
     "-t, --test-match [globPatterns...]",
     "Set the test match glob(s) to detect benchmark tests",
-    collect
+    collect,
+    barbell.defaultConfig.testMatch.slice(0)
   )
   .option(
     "-e, --exclude [globPatterns...]",
     "Exclude specific files and folders",
-    collect
+    collect,
+    barbell.defaultConfig.exclude.slice(0)
   )
   .option(
-    "-s, --stop-on-errors",
+    "-c, --concurrent",
+    "The number of benches you want to run at the same time",
+    undefined,
+    barbell.defaultConfig.concurrent
+  )
+  .option(
+    "-x, --stop-on-errors",
     "Stop Barbell if any errors are found within test suites"
-  );
+  )
+  .option("-v, --verbose", "Verbose mode");
 
 program.action(barbell);
 
